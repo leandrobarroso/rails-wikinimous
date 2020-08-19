641b56f089a058be1cb9e29d5875adde64d0c295
leandro barroso <lrbarroso1984@gmail.com>
2020-08-18 21:35:09 -0300

    Challenge 05-01-03 - Wikinimous - all functionalities working - needs styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  new_article = Article.new(title: Faker::GreekPhilosophers.name, content: Faker::Lorem.paragraph(sentence_count: 3))
+  new_article.save
+end
